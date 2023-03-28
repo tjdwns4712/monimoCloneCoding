8143c0341cfa4000d3039f7176beb95656289508
tjdwns4712 <117796730+tjdwns4712@users.noreply.github.com>
2023-03-28 14:31:40 +0900

    colorChange

--- lib/pages/testPage.dart
+++ lib/pages/testPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,8 @@ class TestPage extends StatefulWidget {
 class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.amber,
+    );
   }
 }
